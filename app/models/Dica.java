36b97a62bca97dbfbad268eb898647166ac91852
AndersonSM <anderson.menezes@ccc.ufcg.edu.br>
2015-03-17 01:02:18 -0300

    UI de Tema

--- app/models/Dica.java
+++ app/models/Dica.java
@@ -1,12 +1,20 @@
 package models;
 
+import java.util.Map;
+
 import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapKeyColumn;
 
 @Entity(name="Dica")
+//@DiscriminatorColumn(name="REF_TYPE")
 public class Dica {
 	@Id
 	@GeneratedValue
@@ -15,5 +23,31 @@ public class Dica {
 	@ManyToOne(cascade=CascadeType.ALL)
 	private Tema tema;
 	
+	@ElementCollection
+    @MapKeyColumn(name="user")
+    @Column(name="commentary")
+    @CollectionTable(name="USERS_COMM", joinColumns=@JoinColumn(name="dica_id"))
+	private Map<String, String> usersCommentaries;
+	
 	public Dica(){}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Map<String, String> getUsersCommentaries() {
+		return usersCommentaries;
+	}
+	
+	public void addUserCommentary(String login, String commentary) {
+		usersCommentaries.put(login, commentary);
+	}
 }
--- app/models/Tema.java
+++ app/models/Tema.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.MapKey;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.OneToMany;
 
 @Entity(name="Tema")
@@ -20,14 +22,18 @@ public class Tema {
 	@Id
 	@GeneratedValue
 	private long id;
+		
+	@Column
+	private String name;
 	
 	@OneToMany(cascade=CascadeType.ALL)
 	@JoinColumn(name="DICAS")
 	private List<Dica> dicas;
 	
-	@ElementCollection(fetch = FetchType.LAZY)
-	@MapKey
-	@Column(name = "USERS_DIFF")
+	@ElementCollection
+    @MapKeyColumn(name="user")
+    @Column(name="dificuldade")
+    @CollectionTable(name="USERS_DIFF", joinColumns=@JoinColumn(name="tema_id"))
 	private Map<String, String> usersDifficulty;
 	
 	@Column(name = "TOTAL_DIFF")
@@ -35,15 +41,47 @@ public class Tema {
 	
 	public Tema(){}
 	
-	public void incrementarDificuldade(String userEmail, int dificuldade) {
-		if (usersDifficulty.containsKey(userEmail)) {
-			String previousDiff = usersDifficulty.get(usersDifficulty);
+	public Tema(String name) {
+		this.name = name;
+	}
+	
+	public void incrementarDificuldade(String userLogin, int dificuldade) {
+		if (usersDifficulty.containsKey(userLogin)) {
+			String previousDiff = usersDifficulty.get(userLogin);
 			
 			this.dificuldade -= Integer.valueOf(previousDiff);
 		}
 		
 		this.dificuldade += dificuldade;
 		
-		usersDifficulty.put(userEmail, String.valueOf(dificuldade));
+		usersDifficulty.put(userLogin, String.valueOf(dificuldade));
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDificuldade() {
+		return dificuldade;
+	}
+
+	public void setDificuldade(int dificuldade) {
+		this.dificuldade = dificuldade;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public List<Dica> getDicas() {
+		return dicas;
+	}
+	
+	public void addDica(Dica dica) {
+		this.dicas.add(dica);
 	}
 }
