c956882923f8ed5ffaca3a9d8d138d497b72b5c2
Leulz <leo.vital@ccc.ufcg.edu.br>
2015-03-21 19:53:59 -0300

    Minha nossa

--- app/models/Dica.java
+++ app/models/Dica.java
@@ -12,6 +12,8 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapKeyColumn;
@@ -19,6 +21,7 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity(name="Dica")
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 //@DiscriminatorColumn(name="REF_TYPE")
 public abstract class Dica implements Comparable<Dica>{
 	@Id
