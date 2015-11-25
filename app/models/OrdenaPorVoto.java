e697e3ec133d3ef285d37669176c4abe8791a149
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2015-11-25 15:40:38 -0300

    Ordenação por maior  num votos funcionando

--- app/models/OrdenaPorVoto.java
+++ app/models/OrdenaPorVoto.java
@@ -12,7 +12,7 @@ public class OrdenaPorVoto implements Ordenavel{
 		while(houveTroca){
 			houveTroca = false;
 			for(int i = 0; i < dicasOrdenada.size() -1; i++){
-				if(dicasOrdenada.get(i).getConcordancias() > (dicasOrdenada.get(i+1).getConcordancias())){
+				if(dicasOrdenada.get(i).getConcordancias() < (dicasOrdenada.get(i+1).getConcordancias())){
 					temp = dicasOrdenada.get(i);
 					dicasOrdenada.set(i,dicasOrdenada.get(i+1) );
 					dicasOrdenada.set(i+1, temp);
